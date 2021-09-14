129018ed95e013bd8413356b5b49855d1f4c92a0
Vivek nautiyal <vivekvn14@gmail.com>
2021-09-14 21:15:49 +0530

    Single Number String

--- src/main/java/com/Incubyte/Assesment/App.java
+++ src/main/java/com/Incubyte/Assesment/App.java
@@ -9,12 +9,25 @@ public class App
 
 	public static int add(String string) {
 		// TODO Auto-generated method stub
+		int sum=0;
 		
 		if(string.length()==0)
 		{
 			return 0;
+		}else
+		{
+			for(int i=0;i<string.length();i++)
+			{
+				if(Character.isDigit(string.charAt(i)))
+						{
+					if(Integer.parseInt(String.valueOf(string.charAt(i)))>0)
+					{
+						sum=sum+Integer.parseInt(String.valueOf(string.charAt(i)));
+					}
+						}
+			}
 		}
-		return 0;
+		return sum;
 	}
     
 }

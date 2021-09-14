d6c2fa0fae292fe7d52f6a4534051c0d7cc43479
Vivek nautiyal <vivekvn14@gmail.com>
2021-09-14 21:09:53 +0530

    Empty String

---
+++ src/main/java/com/Incubyte/Assesment/App.java
@@ -0,0 +1,20 @@
+package com.Incubyte.Assesment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+	public static int add(String string) {
+		// TODO Auto-generated method stub
+		
+		if(string.length()==0)
+		{
+			return 0;
+		}
+		return 0;
+	}
+    
+}
---
+++ src/test/java/com/Incubyte/Assesment/AppTest.java
@@ -0,0 +1,18 @@
+package com.Incubyte.Assesment;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, App.add(""));
+	}
+   
+}
